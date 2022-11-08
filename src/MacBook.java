8681eae5bc5760244517469a79563fc1d85b65a2
Ruslan <ruslan.effect@gmail.com>
2022-11-08 19:53:30 +0600

    8

---
+++ src/MacBook.java
@@ -0,0 +1,33 @@
+public class MacBook {
+    private String model;
+    private String color;
+
+    public MacBook(String model, String color) {
+        this.model = model;
+        this.color = color;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return "MacBook{" +
+                "model='" + model + '\'' +
+                ", color='" + color + '\'' +
+                '}';
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("вывод с мэин класса");
+
+        MacBook mac = new MacBook("MacBook air,m1","space gray");
+
+        System.out.println("вывод из класса макбук " + mac);
+    }
+}
